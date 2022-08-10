32af68a5aa7dfa606957ebd8b118e4e6b5bce880
david cuspoca <david-cuspoca@hotmail.com>
2022-08-09 22:28:51 -0600

    se mejoro el buscador
    ademas se implemento mejoras en el loading del rutero del front

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -10,30 +10,33 @@ const coleccionesPermitidas = [
   'gastos'
 ];
 
-const buscarClientes = async(termino = '', req = request, res = response) => {
-  const esMongoId = ObjectId.isValid(termino);
-  const {ruta} = req.usuario;
 
-  if(esMongoId){
-    const cliente = await ClienteModel.findById(termino);
+const buscarCreditosByName = async(termino = '', req = request, res = response) => {
+  const {ruta} = req.usuario;
+  console.log(termino)
+
+  const allCreditos = await CreditoModel.find({ruta, status: true})
+    .populate('cliente', ['nombre', 'alias', 'direccion', 'ciudad', 'telefono'])
+    .populate('pagos', ['fecha', 'valor'])
+
+  const creditos = allCreditos.filter(c => c.cliente.nombre.includes(termino.toUpperCase()));
+
+  if(termino === 'all'){
     return res.status(200).json({
-      results: (cliente) ? [cliente] : []
+      ok: true,
+      creditos: allCreditos
     })
   }
 
-  const regex = new RegExp(termino, 'i');
-  const clientes = await ClienteModel.find({
-    $or: [{nombre: regex}, {alias: regex}],
-    $and: [{status: true, ruta}]
-  }).populate('creditos')
-
   res.status(200).json({
-    results: clientes
+    ok: true,
+    creditos
   })
+
 }
 
 const buscar = (req, res = response) => {
-  const {coleccion, termino} = req.params;
+  const {coleccion, termino = ''} = req.params;
   if(!coleccionesPermitidas.includes(coleccion)){
     return res.status(400).json({
       msg: `Las colecciones permitidas son ${coleccionesPermitidas}`
@@ -41,8 +44,8 @@ const buscar = (req, res = response) => {
   }
 
   switch (coleccion) {
-    case 'clientes':
-      buscarClientes(termino, req, res)
+    case 'creditos':
+      buscarCreditosByName(termino, req, res)
       break;
   
     default:
