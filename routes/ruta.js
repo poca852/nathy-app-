e3d2f897506145651befb4e4c305945282be9c3d
david cuspoca <david-cuspoca@hotmail.com>
2022-09-09 21:40:31 -0600

    Routes ruta
    Se creo la ruta para agregar una ruta a un administrador

--- routes/ruta.js
+++ routes/ruta.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const {check} = require('express-validator');
-const { postRuta, getRuta, patchRuta, closeRuta, openRuta, getRutas } = require('../controllers/ruta');
+const { postRuta, getRuta, patchRuta, closeRuta, openRuta, getRutas, addRutaAdmin } = require('../controllers/ruta');
 
 // helpers
 const { validarUsuarioById, validarRutaByName, validarRutaById } = require('../helpers/db-validators');
@@ -54,5 +54,15 @@ router.patch('/open/:idRuta', [
   check('idRuta').custom(validarRutaById),
   validarCampos
 ], openRuta);
+
+router.put('/add-ruta-admin/:idAdmin/:idRuta', [
+  validarJWT,
+  esSuperAdmin,
+  check('idAdmin', 'No es un id valido').isMongoId(),
+  check('idAdmin').custom(validarUsuarioById),
+  check('idRuta', 'No es un id valido').isMongoId(),
+  check('idRuta').custom(validarRutaById),
+  validarCampos
+], addRutaAdmin)
 
 module.exports = router;
