d54390dd935aafa2865424877813979773a7a688
david cuspoca <david-cuspoca@hotmail.com>
2022-09-14 08:55:27 -0600

    Cambios en el archivo index de middleware
    se exporta desde index en middlewares el esSuperAdmin

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const validaCampos = require('./validar-campos');
 const validarJWT = require('./validar-jwt');
+const { esSuperAdmin } = require('./validar-roles');
 
 module.exports = {
   ...validaCampos,
   ...validarJWT,
+  esSuperAdmin
 }
