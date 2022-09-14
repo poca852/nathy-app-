7ba407a571e4e3fc7b3a44805605b3677eaf0fd5
david cuspoca <david-cuspoca@hotmail.com>
2022-09-14 08:47:44 -0600

    Revert "Refactorizacion del codigo en los siguientes archivos"
    This reverts commit 67caa5282a4f1b1bb3f6fd4843d1ca35db4e0433.

--- controllers/retiros.js
+++ controllers/retiros.js
@@ -1,38 +1,29 @@
 const { request, response } = require("express");
-
-// modelos
-const { RetiroModel, 
-        RutaModel, 
-        CajaModel } = require('../models');
-        
+const {RetiroModel, RutaModel, CajaModel} = require('../models');
 const moment = require('moment-timezone');
 moment.tz.setDefault('America/Guatemala');
 
-const addRetiro = async (req = request, res = response) => {
+const postRetiro = async (req = request, res = response) => {
+
+  const {ruta} = req.usuario;
+  const body = req.body;
+
   try {
 
-    const { idRuta, fecha, valor, nota } = req.body;
-
     const retiro = await RetiroModel.create({
-      ruta: idRuta,
-      fecha,
-      valor,
-      nota
+      ...body, 
+      ruta
     });
 
-    // Actualizar la ruta y la caja
-    const [rutaModel, cajaActual] = await Promise.all([
-      RutaModel.findById(idRuta),
-      CajaModel.findOne({ruta: idRuta, fecha})
-    ]);
-
-    rutaModel.retiros += valor;
+    const rutaModel = await RutaModel.findById(ruta);
+    rutaModel.retiros += body.valor;
     await rutaModel.save();
 
     // actualizamos la caja
+    const cajaActual = await CajaModel.findById(rutaModel.caja_actual._id)
 
-    cajaActual.retiro += valor;
-    cajaActual.caja_final -= valor;
+    cajaActual.retiro += body.valor;
+    cajaActual.caja_final -= body.valor;
     await cajaActual.save();
 
     res.status(201).json({
@@ -52,19 +43,15 @@ const addRetiro = async (req = request, res = response) => {
 }
 
 const getRetiros = async (req = request, res = response) => {
+
+  const {ruta} = req.usuario;
+
   try {
+    
+    const retiros = await RetiroModel.find({ruta});
 
-    const { idRuta } = req.params;
-
-    const [total, retiros] = await Promise.all([
-      RetiroModel.countDocuments({ ruta: idRuta }),
-      RetiroModel.find({ ruta: idRuta })
-    ])
-
-
-    return res.status(200).json({
+    res.status(200).json({
       ok: true,
-      total,
       retiros
     })
 
@@ -77,111 +64,7 @@ const getRetiros = async (req = request, res = response) => {
   }
 }
 
-const getRetiroById = async (req = request, res = response) => {
-  try {
-
-    const { idRetiro } = req.params;
-
-    const retiro = await RetiroModel.findById(idRetiro);
-
-    return res.status(200).json({
-      ok: true,
-      retiro
-    })
-
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({
-      ok: false,
-      msg: 'Hable con el administrador'
-    })
-  }
-}
-
-const actualizarRetiro = async (req = request, res = response) => {
-  try {
-
-    const { idRetiro } = req.params;
-    const { _id, idRuta, ...resto } = req.body;
-
-    const [retiro, caja, ruta] = await Promise.all([
-      RetiroModel.findById(idRetiro),
-      CajaModel.findOne({ ruta: idRuta, fecha: resto.fecha }),
-      RutaModel.findById(idRuta)
-    ])
-
-    // regresar todo como estaba
-    caja.retiro -= retiro.valor;
-    caja.caja_final += retiro.valor;
-
-    ruta.retiros -= retiro.valor;
-
-    // actualizamos el retiro
-    const retiroUpdated = await RetiroModel.findByIdAndUpdate(idRetiro, resto, { new: true });
-
-    // volver a actualizar los demas valores
-    caja.retiro += resto.valor;
-    caja.caja_final += resto.valor;
-
-    ruta.retiros += resto.valor;
-
-    // guardar los cambios en los modelos involucrador
-    await caja.save();
-    await ruta.save();
-
-    return res.status(200).json({
-      ok: true,
-      retiro: retiroUpdated
-    })
-
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({
-      ok: false,
-      msg: 'Hable con el administrador'
-    })
-  }
-}
-
-const eliminarRetiro = async (req = request, res = response) => {
-  try {
-    const { idRetiro } = req.params;
-    const { fecha, idRuta } = req.body;
-
-    const [retiro, caja, ruta] = await Promise.all([
-      RetiroModel.findById(idRetiro),
-      CajaModel.findOne({ ruta: idRuta, fecha }),
-      RutaModel.findById(idRuta)
-    ])
-
-    //devolver todo como estaba
-    caja.retiro -= retiro.valor;
-    caja.caja_final += retiro.valor;
-    ruta.retiros -= retiro.valor;
-
-    await RetiroModel.findByIdAndDelete(idRetiro);
-
-    await caja.save();
-    await ruta.save();
-
-    return res.status(200).json({
-      ok: true,
-      msg: 'El retiro fue eliminado correctamente'
-    })
-
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({
-      ok: false,
-      msg: 'Hable con el administrador'
-    })
-  }
-}
-
 module.exports = {
-  addRetiro,
-  getRetiros,
-  getRetiroById,
-  actualizarRetiro,
-  eliminarRetiro
+  postRetiro,
+  getRetiros
 }
--- routes/retiros.js
+++ routes/retiros.js
@@ -1,58 +1,19 @@
 const {Router} = require('express');
 const {check} = require('express-validator');
+const { getRetiros, postRetiro } = require('../controllers/retiros');
 
-// controllers 
-const { getRetiros, 
-        addRetiro, 
-        getRetiroById, 
-        actualizarRetiro, 
-        eliminarRetiro } = require('../controllers/retiros');
-
-// middlewares
-const { validarJWT, 
-        validarCampos } = require('../middlewares/');
-
-const { validarExisteRetiroPorId, validarRutaById } = require('../helpers')
+const {validarJWT, validarCampos} = require('../middlewares/');
 
 const router = Router();
 
+router.get('/', [
+  validarJWT,
+], getRetiros);
 
 router.post('/', [
   validarJWT,
-  check('idRuta', 'No es un id valido').isMongoId(),
-  check('idRuta').custom(validarRutaById),
   check('valor', 'no es un valor permitido').isNumeric(),
-  check('fecha', 'Fecha obligatoria').not().isEmpty(),
   validarCampos
-], addRetiro);
-
-// esta ruta es para obtener todos los retiros de una ruta en particular
-router.get('/:idRuta', [
-  validarJWT,
-  check('idRuta', 'No es un id valido').isMongoId(),
-  check('idRuta').custom(validarRutaById),
-  validarCampos
-], getRetiros);
-
-router.get('/:idRetiro', [
-  validarJWT,
-  check('idRetiro', 'No es un id valido').isMongoId(),
-  check('idRetiro').custom(validarExisteRetiroPorId),
-  validarCampos
-], getRetiroById);
-
-router.put('/:idRetiro', [
-  validarJWT,
-  check('idRetiro', 'No es un id valido').isMongoId(),
-  check('idRetiro').custom(validarExisteRetiroPorId),
-  validarCampos
-], actualizarRetiro);
-
-router.delete('/:idRetiro', [
-  validarJWT,
-  check('idRetiro', 'No es un id valido').isMongoId(),
-  check('idRetiro').custom(validarExisteRetiroPorId),
-  validarCampos
-], eliminarRetiro);
+], postRetiro);
 
 module.exports = router;
