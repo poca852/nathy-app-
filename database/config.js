e8e2a867417ad875b26503dd48dfaac3b995abe1
david cuspoca <david-cuspoca@hotmail.com>
2023-03-23 20:00:29 -0600

    correccion de erroes

--- database/config.js
+++ database/config.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const connection = async() => {
    try {
-      await mongoose.connect(process.env.MONGO_URI, {
-         useUnifiedTopology: true
-      });
+      mongoose.set('strictQuery', false);
+      await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
       console.log('db online')
    } catch (error) {
       console.log(error)
       throw new Error('No se pudo inciar la db')
    }
 }
+
 
 module.exports = connection;
